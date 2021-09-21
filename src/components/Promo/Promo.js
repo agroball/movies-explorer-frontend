6557e8b695e9ddca36a562c219f0fde3daecc293
Roman TIMAKOV <timakov_r@M11highway.pro>
2021-09-21 15:48:40 +0300

    1

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,11 +6,11 @@ import weblogo from '../../images/weblogo.svg';
 export const Promo = () => {
   return (
     <section className="promo">
+      <img className="promo__logo" src={weblogo}></img>
       <div className="promo__block">
       <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
       <p className="promo__paragraph">Листайте ниже, чтобы узнать больше про этот проект и его создателя</p>
       </div>
-      <img className="promo__logo" src={weblogo}></img>
       <NavTab />
     </section>
   );
